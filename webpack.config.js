c9d16743ff232140e7b14b286d4e4615092f326c
Mykola Ivanchenko <Mykola.Ivanchenko@AOMV.local>
2022-10-03 20:25:53 +0200

    set up webpack for frontend

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+	mode: 'development',
+	entry: './src/frontend/entry.tsx',
+	output: {
+		path: path.resolve(__dirname, 'dist/frontend'),
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['.js', '.ts', '.tsx']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'ts-loader'
+				}
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			inject: 'body'
+		})
+	]
+}
