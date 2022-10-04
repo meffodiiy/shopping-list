0f0491b7ca9e0c45e52d712a8a5e83114887c6c2
Mykola Ivanchenko <Mykola.Ivanchenko@AOMV.local>
2022-10-04 09:40:17 +0200

    implemented CRUD endpoint for list items

--- src/backend/controllers/item.controller.ts
+++ src/backend/controllers/item.controller.ts
@@ -1,18 +1,45 @@
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import { TItem, TItemWithoutId } from '../types'
+import { HTTPError } from '../middlewares/error.middleware'
+import { Item } from '../models'
+import { v4 } from 'uuid'
 
 
 type TItemId = Pick<TItem, 'id'>
 
 
-export const createItem = (req: Request<{ listId: string }, never, TItemWithoutId>, res: Response) => {
-  return res.sendStatus(200)
+export const createItem = async (req: Request<{ listId: string }, never, TItemWithoutId>, res: Response, next: NextFunction) => {
+  try {
+    const id = v4()
+    await Item.create({
+      id,
+      ...req.params,
+      ...req.body
+    })
+    return res.sendStatus(200)
+  } catch (error) {
+    next(new HTTPError(500, error))
+  }
 }
 
-export const updateItem = (req: Request<TItemId, never, Partial<TItemWithoutId>>, res: Response) => {
-  return res.sendStatus(200)
+export const updateItem = async (req: Request<TItemId, never, Partial<TItemWithoutId>>, res: Response, next: NextFunction) => {
+  try {
+    await Item.update(req.body, {
+      where: req.params
+    })
+    return res.sendStatus(200)
+  } catch (error) {
+    next(new HTTPError(500, error))
+  }
 }
 
-export const deleteItem = (req: Request<TItemId>, res: Response) => {
-  return res.sendStatus(200)
+export const deleteItem = async (req: Request<TItemId>, res: Response, next: NextFunction) => {
+  try {
+    await Item.destroy({
+      where: req.params
+    })
+    return res.sendStatus(200)
+  } catch (error) {
+    next(new HTTPError(500, error))
+  }
 }
